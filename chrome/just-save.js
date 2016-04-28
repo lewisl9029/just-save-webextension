782c2c3384d00dec4670862955994e4933792c0e
Lewis Liu <develop@lewisl.net>
2016-04-28 00:53:13 -0700

    Documenting more issues

--- chrome/just-save.js
+++ chrome/just-save.js
@@ -1,4 +1,10 @@
 'use strict';
+// FIXME: need to keep this on a separate codebase due to
+// need for separate manifest file because persistent attribute is rejected
+// https://bugzilla.mozilla.org/show_bug.cgi?id=1253565
+// should be able to revert to single codebase when Firefox 48 is released
+
+// FIXME: figure out how to prevent toolbar icon from being added in chrome
 
 // adding browser shim for chrome support
 window.browser = chrome;
